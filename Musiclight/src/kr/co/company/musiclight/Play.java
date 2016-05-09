6a1f408d0f52ce2554f003cd971d1489876a95d8
unknown <>
2016-05-09 21:40:54 +0900

    min

--- Musiclight/src/kr/co/company/musiclight/Play.java
+++ Musiclight/src/kr/co/company/musiclight/Play.java
@@ -13,22 +13,23 @@ public class Play extends Activity{
 	MediaPlayer mp = null;
 	TextView v;
 	String s;
-	Button start;
-	Button pause;
+	Button start, pause;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.player);
+		
 		Intent intent = getIntent();
 		String path = intent.getExtras().getString("path");
 		String fileName = intent.getExtras().getString("fileName");
+		
 		start = (Button) findViewById(R.id.start);
 		pause = (Button) findViewById(R.id.pause);
 		
 		s= path+fileName;
 		
-		((TextView) findViewById(R.id.mypath)).setText("경로: "+path+fileName);	
+		((TextView) findViewById(R.id.mypath)).setText("경로: "+ s);	
 		
 		start.setOnClickListener(new Button.OnClickListener(){
 			public void onClick(View v){
@@ -55,20 +56,6 @@ public class Play extends Activity{
 			}
 		});
 		}
-				
-	
-
-	
-	
-	
-	
-	public void stopFileAudio(View v){
-		if(mp != null){
-			mp.stop();
-			mp.release();
 			
-		}
-		mp=null;
-	}
-
+	
 }
