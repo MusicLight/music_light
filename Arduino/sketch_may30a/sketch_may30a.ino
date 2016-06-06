903fe817613ccac5fda067c8cf21897856f06171
unknown <>
2016-06-07 05:26:34 +0900

    min

--- Arduino/sketch_may30a/sketch_may30a.ino
+++ Arduino/sketch_may30a/sketch_may30a.ino
@@ -1,3 +1,9 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(8, 9); // SoftwareSerial(RX, TX)
+byte buffer[1024]; // 데이터를 수신 받을 버퍼
+int bufferPosition; // 버퍼에 데이타를 저장할 때 기록할 위치
+boolean temp = 0;
 int relay1 = 2;
 int relay2 = 3;
 int relay3 = 4;
@@ -5,34 +11,40 @@ int relay4 = 5;
 
 
 void setup() {
+  BTSerial.begin(9600);
   Serial.begin(9600);
+  bufferPosition = 0;
 
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
   pinMode(relay4, OUTPUT);
 
-  delay(5000);
 }
 
 
 
 void loop() {
 
-  Serial.println("HIGH");
-  digitalWrite(relay1, HIGH); // HIGH인가
-  delay(2000);
+  if (BTSerial.available()) { // 블루투스로 데이터 수신
+    byte data = BTSerial.read(); // 수신 받은 데이터 저장
+    Serial.write(data); // 수신된 데이터 시리얼 모니터로 출력
+    buffer[bufferPosition++] = data; // 수신 받은 데이터를 버퍼에 저장
 
-  Serial.println("LOW");
-  digitalWrite(relay1, LOW); // LOW인가
-  delay(2000);
+    if (data == 'a') { // 블루투스를 통해 'a' 이 들어오면
+      if (temp == 0) {
+        digitalWrite(relay1, HIGH);
+        temp = 1;
+      } else {
+        digitalWrite(relay1, LOW);
+        temp = 0;
+      }
+    }
 
-  Serial.println("HIGH");
-  digitalWrite(relay2, HIGH); // HIGH인가
-  delay(2000);
-
-  Serial.println("LOW");
-  digitalWrite(relay2, LOW); // LOW인가
-  delay(2000);
+    if (data == '\n') { // 문자열 종료 표시
+      buffer[bufferPosition] = '\0';
+      bufferPosition = 0;
+    }
+  }
 
 }
--- MusicLight/gen/kr/co/company/musiclight/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package kr.co.company.musiclight;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
