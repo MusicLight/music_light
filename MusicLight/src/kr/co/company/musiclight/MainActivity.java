f163db0a057b7e27e99411ab4ce7b26221fa05ec
USER <USER@192.168.0.106>
2016-07-20 11:07:43 +0900

    jin

--- MusicLight/src/kr/co/company/musiclight/MainActivity.java
+++ MusicLight/src/kr/co/company/musiclight/MainActivity.java
@@ -7,28 +7,7 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-<<<<<<< HEAD
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
 
-      /*
-       * @Override public boolean onCreateOptionsMenu(Menu menu) {
-       * MenuInflater inflater = getMenuInflater();
-       * inflater.inflate(R.menu.option_menu, menu); return true; }
-       * 
-       * @Override public boolean onOptionsItemSelected(MenuItem item) {
-       * switch (item.getItemId()) { case R.id.about: // Show info about the
-       * author (that's me!) aboutAlert.show(); return true; } return false; }
-       */
-   }
-
-   public void FileListener(View target) {
-      Intent intent = new Intent(getApplicationContext(), FileExplorer.class);
-      startActivity(intent);
-   }
-=======
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,5 +18,5 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(getApplicationContext(), FileExplorer.class);
 		startActivity(intent);
 	}
->>>>>>> 950a28ea47caa5c1b5cdc9ac84f34916fdc8ac35
+
 }
