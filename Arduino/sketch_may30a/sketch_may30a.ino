ca6b3d314307ac1d60c0036f74c068b86463b546
unknown <>
2016-07-23 16:51:45 +0900

    min

--- Arduino/sketch_may30a/sketch_may30a.ino
+++ Arduino/sketch_may30a/sketch_may30a.ino
@@ -11,6 +11,9 @@ int lay1State = 0;
 int lay2State = 0;
 int lay3State = 0;
 
+char buffer[1000];
+char bufferIndex = 0;
+
 void setup() {
   BTSerial.begin(9600);
   Serial.begin(9600);
@@ -24,49 +27,93 @@ void setup() {
 
 void loop()
 {
-
   if (BTSerial.available()) {
-    byte data  =  BTSerial.read();
+    //int data  =  BTSerial.read();
+    buffer[bufferIndex] = BTSerial.read();
+    //bufferIndex++;
+    int data = atoi(buffer);
 
-    Serial.write(data);
+    Serial.print(data);
 
-    if (-128 <= data < -40) {
-      if (lay1State == 0) { // 릴레이1이 꺼져있을 경우
-        digitalWrite(relay1, HIGH);
-        lay1State = 1; // 릴레이1 상태를 켜진 상태로 세팅
-        delay(150);
-      }
-      else { // 릴레이1이 켜져있을 경우
-        digitalWrite(relay1, LOW);
-        lay1State = 0;
-        delay(150);
-      }
+    switch (data) {
+      case 0 ... 3 :
+        if (lay1State == 0) { // 릴레이1이 꺼져있을 경우
+          digitalWrite(relay1, HIGH);
+          lay1State = 1; // 릴레이1 상태를 켜진 상태로 세팅
+          delay(150);
+        }
+        else { // 릴레이1이 켜져있을 경우
+          digitalWrite(relay1, LOW);
+          lay1State = 0;
+          delay(150);
+        }
+        break;
+
+      case 4 ... 6 :
+        if (lay2State == 0) { // 릴레이2이 꺼져있을 경우
+          digitalWrite(relay2, HIGH);
+          lay2State = 1; // 릴레이2 상태를 켜진 상태로 세팅
+          delay(150);
+        }
+        else { // 릴레이2이 켜져있을 경우
+          digitalWrite(relay2, LOW);
+          lay2State = 0;
+          delay(150);
+        }
+        break;
+
+      case 7 ... 9 :
+        if (lay3State == 0) { // 릴레이3이 꺼져있을 경우
+          digitalWrite(relay3, HIGH);
+          lay3State = 1; // 릴레이3 상태를 켜진 상태로 세팅
+          delay(150);
+        }
+        else { // 릴레이3이 켜져있을 경우
+          digitalWrite(relay3, LOW);
+          lay3State = 0;
+          delay(150);
+        }
+        break;
     }
 
-    if (-40 <= data < 40) {
-      if (lay2State == 0) { // 릴레이2이 꺼져있을 경우
-        digitalWrite(relay2, HIGH);
-        lay2State = 1; // 릴레이2 상태를 켜진 상태로 세팅
-        delay(150);
-      }
-      else { // 릴레이2이 켜져있을 경우
-        digitalWrite(relay2, LOW);
-        lay2State = 0;
-        delay(150);
-      }
-    }
 
-    if (data >= 40) {
-      if (lay3State == 0) { // 릴레이3이 꺼져있을 경우
-        digitalWrite(relay3, HIGH);
-        lay3State = 1; // 릴레이3 상태를 켜진 상태로 세팅
-        delay(150);
-      }
-      else { // 릴레이3이 켜져있을 경우
-        digitalWrite(relay3, LOW);
-        lay3State = 0;
-        delay(150);
-      }
-    }
-  } //연속적으로 릴레이는 켜지나, 배열받는것은 아직
+    /*   if (0 <= data < 3) {
+         if (lay1State == 0) { // 릴레이1이 꺼져있을 경우
+           digitalWrite(relay1, HIGH);
+           lay1State = 1; // 릴레이1 상태를 켜진 상태로 세팅
+           delay(150);
+         }
+         else { // 릴레이1이 켜져있을 경우
+           digitalWrite(relay1, LOW);
+           lay1State = 0;
+           delay(150);
+         }
+        }
+
+          if (3 <= data < 5) {
+            if (lay2State == 0) { // 릴레이2이 꺼져있을 경우
+              digitalWrite(relay2, HIGH);
+              lay2State = 1; // 릴레이2 상태를 켜진 상태로 세팅
+              delay(150);
+            }
+            else { // 릴레이2이 켜져있을 경우
+              digitalWrite(relay2, LOW);
+              lay2State = 0;
+              delay(150);
+            }
+          }
+
+          if (data >= 5) {
+            if (lay3State == 0) { // 릴레이3이 꺼져있을 경우
+              digitalWrite(relay3, HIGH);
+              lay3State = 1; // 릴레이3 상태를 켜진 상태로 세팅
+              delay(150);
+            }
+            else { // 릴레이3이 켜져있을 경우
+              digitalWrite(relay3, LOW);
+              lay3State = 0;
+              delay(150);
+            }
+          } */
+  }    //연속적으로 릴레이는 켜지나, 배열받는것은 아직
 }
