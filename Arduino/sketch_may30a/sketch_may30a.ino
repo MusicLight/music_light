695360e3cbf089d0140a780becd4c65f8adbc56b
unknown <>
2016-08-08 00:39:33 +0900

    배열은 읽음

--- Arduino/sketch_may30a/sketch_may30a.ino
+++ Arduino/sketch_may30a/sketch_may30a.ino
@@ -27,80 +27,87 @@ void setup() {
   pinMode(relay4, OUTPUT);
   digitalWrite(relay4, HIGH);
 }
-
+int i=0;
 
 void loop()
 {
-  if (BTSerial.available()) {
-    //int data  =  BTSerial.read();
-    buffer[bufferIndex] = BTSerial.read();
-    //bufferIndex++;
-    int data = atoi(buffer);
+  byte recv[1000];
+  int recvchar;
+  int count = BTSerial.available();
+  if (count) {
+    recvchar =  BTSerial.read();
+    recv[i] = recvchar;
+    Serial.println(recv[i]);
+    i++;
+    /* if (BTSerial.available()) {
+       //int data  =  BTSerial.read();
+       buffer[bufferIndex] = BTSerial.read();
+       //bufferIndex++;
+       int data = atoi(buffer);
 
-    Serial.print(data);
-
-    switch (data) {
-      case 0 ... 30 :
-        digitalWrite(relay1, LOW);
-        delay(150);
-        digitalWrite(relay1, HIGH);
-        delay(150);
-        break;
-
-      case 31 ... 60 :
-        digitalWrite(relay2, LOW);
-        delay(150);
-        digitalWrite(relay2, HIGH);
-        delay(150);
-        break;
-
-      case 61 ... 900 :
-        digitalWrite(relay3, LOW);
-        delay(150);
-        digitalWrite(relay3, HIGH);
-        delay(150);
-        break;
-
-    }
-
-
-    /*   if (0 <= data < 3) {
-         if (lay1State == 0) { // 릴레이1이 꺼져있을 경우
-           digitalWrite(relay1, HIGH);
-           lay1State = 1; // 릴레이1 상태를 켜진 상태로 세팅
-           delay(150);
-         }
-         else { // 릴레이1이 켜져있을 경우
+       Serial.print(data);
+*/
+       switch (recv[i]) {
+         case 0 ... 5 :
            digitalWrite(relay1, LOW);
-           lay1State = 0;
            delay(150);
-         }
-        }
+           digitalWrite(relay1, HIGH);
+           delay(150);
+           break;
 
-          if (3 <= data < 5) {
-            if (lay2State == 0) { // 릴레이2이 꺼져있을 경우
-              digitalWrite(relay2, HIGH);
-              lay2State = 1; // 릴레이2 상태를 켜진 상태로 세팅
-              delay(150);
-            }
-            else { // 릴레이2이 켜져있을 경우
-              digitalWrite(relay2, LOW);
-              lay2State = 0;
-              delay(150);
-            }
-          }
+         case 6 ... 10 :
+           digitalWrite(relay2, LOW);
+           delay(150);
+           digitalWrite(relay2, HIGH);
+           delay(150);
+           break;
 
-          if (data >= 5) {
-            if (lay3State == 0) { // 릴레이3이 꺼져있을 경우
-              digitalWrite(relay3, HIGH);
-              lay3State = 1; // 릴레이3 상태를 켜진 상태로 세팅
+         case 11 ... 101 :
+           digitalWrite(relay3, LOW);
+           delay(150);
+           digitalWrite(relay3, HIGH);
+           delay(150);
+           break;
+
+
+       }
+
+
+       /*   if (0 <= data < 3) {
+            if (lay1State == 0) { // 릴레이1이 꺼져있을 경우
+              digitalWrite(relay1, HIGH);
+              lay1State = 1; // 릴레이1 상태를 켜진 상태로 세팅
               delay(150);
             }
-            else { // 릴레이3이 켜져있을 경우
-              digitalWrite(relay3, LOW);
-              lay3State = 0;
+            else { // 릴레이1이 켜져있을 경우
+              digitalWrite(relay1, LOW);
+              lay1State = 0;
               delay(150);
             }
-          } */
-  }    // 배열을 받긴 하지만 배열안에 있는 값을 못읽는상태 ( 다 0으로 읽음 )
+           }
+             if (3 <= data < 5) {
+               if (lay2State == 0) { // 릴레이2이 꺼져있을 경우
+                 digitalWrite(relay2, HIGH);
+                 lay2State = 1; // 릴레이2 상태를 켜진 상태로 세팅
+                 delay(150);
+               }
+               else { // 릴레이2이 켜져있을 경우
+                 digitalWrite(relay2, LOW);
+                 lay2State = 0;
+                 delay(150);
+               }
+             }
+             if (data >= 5) {
+               if (lay3State == 0) { // 릴레이3이 꺼져있을 경우
+                 digitalWrite(relay3, HIGH);
+                 lay3State = 1; // 릴레이3 상태를 켜진 상태로 세팅
+                 delay(150);
+               }
+               else { // 릴레이3이 켜져있을 경우
+                 digitalWrite(relay3, LOW);
+                 lay3State = 0;
+                 delay(150);
+               }
+             } */
+  }    // 배열값은 읽음
 }
