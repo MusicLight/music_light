eb7f4b020d1729b78606ba5d11869c90f02609fd
unknown <>
2016-10-03 22:34:40 +0900

    원상복구

--- Jin/src/com/example/jin/MainActivity.java
+++ Jin/src/com/example/jin/MainActivity.java
@@ -16,34 +16,8 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-<<<<<<< HEAD
-		
 	}
-	
-	
-	
-	
-=======
-		connect_button = (Button) findViewById(R.id.connect_button);
-	
-		connect_button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (connectStat) {
-					// Attempt to disconnect from the device
-					disconnect();
-				} else {
-					// Attempt to connect to the device
-					connect();
-				}
-			}
-		});
-	}
-	
-	
 
->>>>>>> 17753f3a9b4ca3e79e794619b0ed7660ebaeed6b
 	public void FileListener(View target) {
 		Intent intent = new Intent(getApplicationContext(), FileExplorer.class);
 		startActivity(intent);
